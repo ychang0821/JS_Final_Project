d81c4015d14fdd75426e1a850e53a418f298bd2f
jderaymond <jusderay@amazon.com>
2021-07-22 17:22:14 -0400

    Added event listener for search bar to populate desired weather and events

--- HomePage/homepage_script.js
+++ HomePage/homepage_script.js
@@ -1,10 +1,27 @@
-const WEATHER_API_URL = 'https://api.weatherbit.io/v2.0/forecast/daily?city=london&key=c4b16440bcb0427b9cc88cdce6d66263&days=7&units=I';
+const WEATHER_API_URL = 'https://api.weatherbit.io/v2.0/forecast/daily?';
 const EVENT_API_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?';
+const WEATHER_API_KEY = 'c4b16440bcb0427b9cc88cdce6d66263'
 const EVENT_API_KEY ='ULnge4RhAQVQjajspAYnv87RNIGGUZI7';
 
-getNearbyEvents('seattle')
-/*
-fetch(WEATHER_API_URL)
+document.getElementById("city-form").addEventListener("submit", handleSubmit) 
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const searchCity = e.target.q.value;
+
+    console.log(searchCity);
+
+    e.target.q.value = "";
+
+    document.getElementById('eventCard_container').innerHTML = "";
+    document.getElementById('weatherCard_container').innerHTML = "";
+    
+    getNearbyEvents(searchCity);
+    getWeather(searchCity);
+
+}
+function getWeather(city){
+fetch(`${WEATHER_API_URL}city=${city}&key=${WEATHER_API_KEY}&days=7&units=I`)
     .then(res => res.json())
     .then(result => {
         weather_object = {};
@@ -42,7 +59,8 @@ fetch(WEATHER_API_URL)
             document.getElementById("weatherCard_container").appendChild(weatherCard);
         }
     })
-*/
+}
+
     function getNearbyEvents(city) {
         //add country parameter
         //insert if statement to check if country is USA
@@ -51,7 +69,7 @@ fetch(WEATHER_API_URL)
         .then(res => res.json())
         .then(result => {
             event_object = {};
-            for(let i = 0; i<=4; i++) {
+            for(let i = 0; i<=2; i++) {
                 const name = result._embedded.events[i].name;
                 const date = result._embedded.events[i].dates.start.localDate;
                 const time = result._embedded.events[i].dates.start.localTime;
